f434d4dab50e932fe349394229861c94526a2169
SnowFlash383935 <snowflash383935@mail.ru>
2026-07-25 21:22:21 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
     );
 
     engine.loadFromModule("io.fleshka.tunes", "Main");
-
+    if (engine.rootObjects().isEmpty())
+        return -1;
     return app.exec();
 }
