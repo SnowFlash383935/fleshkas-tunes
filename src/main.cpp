6084be775aa5a676d8483c74e31c75b684747d1c
SnowFlash383935 <snowflash383935@mail.ru>
2026-07-25 17:11:22 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,20 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.loadFromModule("io.fleshka.tunes", "Main");
 
-    if (engine.rootObjects().isEmpty())
-        return -1;
+    const QUrl url(u"qrc:/io.fleshka.tunes/qml/Main.qml"_qs);
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() {
+            QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection
+    );
+
+    engine.load(url);
 
     return app.exec();
 }
