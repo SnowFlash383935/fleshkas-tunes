0c1518ddebf227f75bf5ec191ee0fe0854a4bf61
SnowFlash383935 <snowflash383935@mail.ru>
2026-07-25 19:49:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -13,32 +12,12 @@ int main(int argc, char *argv[])
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() {
-            qCritical() << "QML object creation failed!";
             QCoreApplication::exit(-1);
         },
         Qt::QueuedConnection
     );
 
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::warnings,
-        [](const QList<QQmlError> &warnings) {
-            for (const auto &warning : warnings) {
-                qWarning().noquote() << warning.toString();
-            }
-        }
-    );
-
-    engine.load(
-        QUrl(QStringLiteral(
-            "qrc:/qt/qml/io/fleshka/tunes/qml/Main.qml"
-        ))
-    );
-
-    if (engine.rootObjects().isEmpty()) {
-        qCritical() << "No root QML objects were created!";
-        return -1;
-    }
+    engine.loadFromModule("io.fleshka.tunes", "Main");
 
     return app.exec();
 }
